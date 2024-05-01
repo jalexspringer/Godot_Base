2f176437ed9abb3fbb2cd7e6822c925b3a053a53
Alex Springer <alexspringer@pm.me>
2024-05-01 15:58:19 +0100

    clean basic hexagon world gen ready for voronoi oceans

--- scripts/global/world_generator.gd
+++ scripts/global/world_generator.gd
@@ -3,9 +3,7 @@ extends Node
 const TILE_COUNT = 5000
 const OCEAN_COUNT = 3
 const TILE_SIZE = 64 # Tile size in pixels
-# const HEX_POINTS: Array[Vector2] = [Vector2(32, 0), Vector2(16, 27.71281), Vector2( - 16, 27.71281), Vector2( - 32, 0), Vector2( - 16, -27.71281), Vector2(16, -27.71281)]
 
-# var _planet: Planet
 var _world_map: Dictionary
 var _climate_zones: Array[ClimateZone]
 var hex_points: Array[Vector2]
@@ -14,8 +12,6 @@ var hex_points: Array[Vector2]
 
 func _ready() -> void:
 	_load_climate_zones()
-	#hex_points = _create_points()
-	#print(hex_points)
 
 func _load_climate_zones() -> void:
 	_climate_zones = []
@@ -24,11 +20,8 @@ func _load_climate_zones() -> void:
 		if resource is ClimateZone:
 			_climate_zones.append(resource)
 
-func generate_world(planet: Planet) -> void:
-	print(planet.name)
-	#var voronoi_points := generate_voronoi_points(100, TILE_COUNT)
-	_world_map = build_world_map(TILE_COUNT) # , voronoi_points)
-	#_world_map = assign_oceans_to_world_map(_world_map, voronoi_points, 70.0)
+func generate_world(_planet: Planet) -> void:
+	_world_map = build_world_map(TILE_COUNT)
 
 func build_world_map(num_tiles: int) -> Dictionary:
 	var world_map := {}
@@ -59,7 +52,7 @@ func build_world_map(num_tiles: int) -> Dictionary:
 			var troposphere := AirMass.new()
 
 			# Calculate the base temperature using a placeholder function
-			var base_temp := calculate_base_temp(coords)
+			var base_temp := _calculate_base_temp(coords)
 
 			# Create a new instance of the WorldTile class
 			var tile := WorldTile.new(
@@ -80,102 +73,7 @@ func build_world_map(num_tiles: int) -> Dictionary:
 
 	return world_map
 
-func _create_points() -> Array[Vector2]:
-	var points: Array[Vector2] = []
-	var num_sides: int = 6
-	var angle_step: float = 2 * PI / num_sides
-	var radius: float = TILE_SIZE / 2.0
-
-	for i in range(num_sides):
-		var angle: float = angle_step * i
-		points.append(Vector2(cos(angle), sin(angle)) * radius)
-
-	return points
-
 # Placeholder function to calculate the base temperature based on coordinates
-func calculate_base_temp(_coords: Vector2) -> float:
+func _calculate_base_temp(_coords: Vector2) -> float:
 	# Implement your temperature calculation logic here
 	return 0.0
-
-func generate_voronoi_points(num_points: int, num_tiles: int) -> Array[Vector2]:
-	var points = []
-	for z in num_points:
-		var x = randf() * sqrt(num_tiles)
-		var y = randf() * sqrt(num_tiles)
-		points.append(Vector2(x, y))
-	return points
-
-func generate_and_assign_voronoi_diagram(world_map: Dictionary, voronoi_points: Array[Vector2]) -> Dictionary:
-	# Generate Voronoi diagram using Bowyer-Watson algorithm
-	var _voronoi_diagram = generate_voronoi_diagram_bowyer_watson(voronoi_points)
-
-	# Assign Voronoi cells to tiles
-	for coords in world_map.keys():
-		var cell_index = find_closest_voronoi_point(coords, voronoi_points)
-		world_map[coords].voronoi_cell = cell_index
-
-	return world_map
-
-func generate_voronoi_diagram_bowyer_watson(points: Array[Vector2]) -> Dictionary:
-	var voronoi_diagram = {}
-	var triangulation = Geometry2D.triangulate_delaunay(PackedVector2Array(points))
-	for i in range(0, triangulation.size(), 3):
-		var p1 = points[triangulation[i]]
-		var p2 = points[triangulation[i + 1]]
-		var p3 = points[triangulation[i + 2]]
-		var circumcenter = calculate_circumcenter(p1, p2, p3)
-		for j in range(3):
-			var vertex_index = triangulation[i + j]
-			if not voronoi_diagram.has(vertex_index):
-				voronoi_diagram[vertex_index] = []
-			voronoi_diagram[vertex_index].append(circumcenter)
-	return voronoi_diagram
-
-func assign_oceans_to_world_map(world_map: Dictionary, voronoi_points: Array[Vector2], ocean_percentage: float) -> Dictionary:
-	# Determine ocean tiles based on ocean percentage
-	var num_ocean_cells = int(voronoi_points.size() * ocean_percentage / 100.0)
-	var ocean_cells = []
-	for z in num_ocean_cells:
-		var random_index = randi() % voronoi_points.size()
-		ocean_cells.append(random_index)
-
-	# Set is_ocean property for tiles based on Voronoi cells
-	for coords in world_map.keys():
-		var cell_index = world_map[coords].voronoi_cell
-		world_map[coords].is_ocean = cell_index in ocean_cells
-
-	return world_map
-
-func calculate_circumcenter(p1: Vector2, p2: Vector2, p3: Vector2) -> Vector2:
-	var d = 2 * (p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y))
-	var x = ((p1.x * p1.x + p1.y * p1.y) * (p2.y - p3.y) + (p2.x * p2.x + p2.y * p2.y) * (p3.y - p1.y) + (p3.x * p3.x + p3.y * p3.y) * (p1.y - p2.y)) / d
-	var y = ((p1.x * p1.x + p1.y * p1.y) * (p3.x - p2.x) + (p2.x * p2.x + p2.y * p2.y) * (p1.x - p3.x) + (p3.x * p3.x + p3.y * p3.y) * (p2.x - p1.x)) / d
-	return Vector2(x, y)
-
-func find_closest_voronoi_point(coords: Vector2, voronoi_points: Array[Vector2]) -> int:
-	var min_distance = INF
-	var closest_index = -1
-	for i in voronoi_points.size():
-		var distance = coords.distance_to(voronoi_points[i])
-		if distance < min_distance:
-			min_distance = distance
-			closest_index = i
-	return closest_index
-
-func assign_ocean_tiles(world_map: Dictionary, num_points: int, ocean_percentage: float) -> Dictionary:
-	# Mark ocean cells based on ocean_percentage
-	var num_ocean_cells = int(num_points * ocean_percentage / 100.0)
-	var ocean_cells = []
-	for z in num_ocean_cells:
-		var random_index = randi() % num_points
-		ocean_cells.append(random_index)
-
-	# Set is_ocean property for tiles based on Voronoi cells
-	for coords in world_map.keys():
-		var cell_index = world_map[coords].voronoi_cell
-		if cell_index in ocean_cells:
-			world_map[coords].is_ocean = true
-		else:
-			world_map[coords].is_ocean = false
-
-	return world_map
--- scripts/world_tile.gd
+++ scripts/world_tile.gd
@@ -14,6 +14,9 @@ extends Node2D
 @onready var moisture_value = get_node("/root/World/WorldUI/TileInfoPanel/VBoxContainer/GridContainer/MoistureValue")
 @onready var is_ocean_value = get_node("/root/World/WorldUI/TileInfoPanel/VBoxContainer/GridContainer/IsOceanValue")
 
+func _ready() -> void:
+    update_hex_polygon_color()
+
 func populate_ui_panel():
     coordinates_value.text = str(tile_resource.coordinates)
     climate_zone_value.text = tile_resource.climate_zone_name
@@ -22,6 +25,23 @@ func populate_ui_panel():
     temperature_value.text = str(tile_resource.base_temperature)
     moisture_value.text = str(tile_resource.moisture)
     is_ocean_value.text = str(tile_resource.is_ocean)
+
+func update_hex_polygon_color():
+    if not tile_resource.is_ocean:
+        hex_polygon.color = Color.GREEN
+
+func __create_points() -> Array[Vector2]:
+    ## TODO : This function creates the points for the polygon2d that makes the tile shape. It uses the constants in the global scope. It should be made dynamic, maybe by having it as part of the constructor for @world_tile.gd
+    var points: Array[Vector2] = []
+    var num_sides: int = 6
+    var angle_step: float = 2 * PI / num_sides
+    var radius: float = WorldGenerator.TILE_SIZE / 2.0
+
+    for i in range(num_sides):
+        var angle: float = angle_step * i
+        points.append(Vector2(cos(angle), sin(angle)) * radius)
+
+    return points
 
 func _on_area_2d_mouse_exited() -> void:
     outline.default_color = Color.BLACK
