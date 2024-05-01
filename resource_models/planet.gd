0c418be14722701d6dd0bc4157db632d094c5d23
Alex Springer <alexspringer@pm.me>
2024-05-01 10:58:54 +0100

    cleaner world gen screen

--- resource_models/planet.gd
+++ resource_models/planet.gd
@@ -14,6 +14,8 @@ const STEFAN_BOLTZMANN_CONSTANT = 5.670374419e-8  # Stefan-Boltzmann constant in
 @export_range(0.5, 1.5) var magnetic_field_strength: float = 1.0  # Magnetic field strength relative to Earth's
 @export_range(200, 500) var orbital_period: float = 365.25  # Orbital period in Earth days
 @export_range(10, 40) var day_length: float = 24.0  # Day length in Earth hours
+@export_range(0, 100) var ocean_coverage_percentage: float = 70.0  # Percentage of surface area covered by oceans
+@export_range(1, 5) var elevation_change: int = 3  # Level of mountainous terrain (1-5, 5 being most mountains)
 
 var average_temperature: float = 0.0
 var spin_speed: float = 0.0
@@ -21,7 +23,8 @@ var average_wind_speed: float = 0.0
 
 func _init(p_name: String = "", p_solar_constant: float = 1361.0, p_radius: float = 6371.0, p_distance_from_sun: float = 1.0,
             p_axial_tilt: float = 23.5, p_albedo: float = 0.3, p_gravity: float = 1.0, p_atmospheric_pressure: float = 1.0,
-            p_magnetic_field_strength: float = 1.0, p_orbital_period: float = 365.25, p_day_length: float = 24.0) -> void:
+            p_magnetic_field_strength: float = 1.0, p_orbital_period: float = 365.25, p_day_length: float = 24.0,
+            p_ocean_coverage_percentage: float = 70.0, p_elevation_change: int = 3) -> void:
     name = p_name
     solar_constant = p_solar_constant
     radius = p_radius
@@ -33,6 +36,8 @@ func _init(p_name: String = "", p_solar_constant: float = 1361.0, p_radius: floa
     magnetic_field_strength = p_magnetic_field_strength
     orbital_period = p_orbital_period
     day_length = p_day_length
+    ocean_coverage_percentage = p_ocean_coverage_percentage
+    elevation_change = p_elevation_change
     _calculate_derived_properties()
 
 func _calculate_derived_properties() -> void:
